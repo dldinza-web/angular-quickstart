8bf76221c3430e091f80416fe6ec8093434ef214
Dennys Lopez Dinza <dldinza.web@gmail.com>
2016-12-06 07:53:41 -0300

    AngularJS QuickStart

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: 'quickstart-app',
+  template: `<h1>Hello {{title}}</h1>`
+})
+
+export class AppComponent {
+  title = 'MEJORVE';
+}
